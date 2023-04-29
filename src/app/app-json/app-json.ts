c9ab857ad0ed22cef6583290e496726ce236a54b
joeylam <joey.cm.lam@gmail.com>
2023-04-29 15:16:52 +0800

    support the nested json and write to excel.

--- src/app/app-json/app-json.ts
+++ src/app/app-json/app-json.ts
@@ -1,11 +1,11 @@
 import JsonHelper from "./jsonHelper";
 
 class app {
-    static process() {
+    static async process() {
         console.log('main - start');
         const a = new JsonHelper();
-        a.processJsonfile('data/test.json');
-        a.write2excel('output/json.xlsx', 'sheet1');
+        await a.processJsonfile('data/test.json');
+        await a.write2excel('output/json.xlsx', 'sheet1');
         console.log('main - done');
     }
 }
--- src/app/app-json/jsonHelper.ts
+++ src/app/app-json/jsonHelper.ts
@@ -1,46 +1,71 @@
 import * as fs from 'fs';
 import {Workbook, Worksheet} from "exceljs";
-import {typeHelpder} from "../lib/typeHelper";
 
 
 export default class JsonHelper {
 
 
     private workbook = new Workbook();
-
-    private _data: any;
-    private _fileName: string = '';
-
-    get data() {
-        return this._data;
-    }
+    private data: any[] = [];
+    private inFileName: string = '';
+    private outFileName: string = '';
 
     constructor() {
 
     }
 
     public async processJsonfile(fileName: string) {
-        this._fileName = fileName;
+        this.inFileName = fileName;
         await this.readJson();
     }
     private async readJson(): Promise<void> {
-        const context = fs.readFileSync(this._fileName, 'utf8');
-        this._data = await JSON.parse(context);
+        const context = fs.readFileSync(this.inFileName, 'utf8');
+        this.data = await JSON.parse(context);
+        console.log(`row count: [${this.data.length}]`)
     }
 
 
-    private async writeContext(worksheet: Worksheet): Promise<void> {
-        if (typeHelpder.isNull(this.data)) { return; }
 
-        for (let record of this._data) {
-            worksheet.addRow(record);
+    private flatten(obj: any, prefix: string = '', result: any = {}): any {
+        return Object.entries(obj).reduce((accumulator, [key, value]) => {
+            const newKey = prefix ? `${prefix}.${key}` : key;
+
+            if (typeof value === 'object' && value !== null) {
+                return this.flatten(value, newKey, accumulator);
+            } else {
+                accumulator[newKey] = value;
+                return accumulator;
+            }
+        }, result);
+    }
+
+
+    private async writeHeader(worksheet: Worksheet): Promise<void> {
+        if (this.data.length === 0) { return; }
+
+        const headerRow = this.data[0];
+        const flattenedHeaderRow = this.flatten(headerRow);
+
+        worksheet.columns = Object.keys(flattenedHeaderRow).map((key) => {
+            return {header: key, key: key};
+        });
+    }
+
+    private async writeContext(worksheet: Worksheet): Promise<void> {
+
+
+        await this.writeHeader(worksheet);
+        for (const item of this.data) {
+            const flattenedItem = this.flatten(item)
+            worksheet.addRow(flattenedItem);
         }
+        this.workbook.xlsx.writeFile(this.outFileName);
     }
 
     public async write2excel(outputFile: string, sheetName: string): Promise<void> {
         const worksheet= this.workbook.addWorksheet(sheetName);
+        this.outFileName = outputFile;
         await this.writeContext(worksheet);
-        await this.workbook.xlsx.writeFile(outputFile);
     }
 
 
