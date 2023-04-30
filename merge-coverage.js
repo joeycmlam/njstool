567bc3864bad42cb00a69385b0d4775fda8de83f
joeylam <joey.cm.lam@gmail.com>
2023-04-30 22:25:29 +0800

    fix the testing reports

---
+++ merge-coverage.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const { createCoverageMap } = require('istanbul-lib-coverage');
+const { createReporter } = require('istanbul-api');
+
+const coverageJestDir = path.join(__dirname, 'coverage-jest');
+const coverageCucumberDir = path.join(__dirname, 'coverage-cucumber');
+
+const coverageMap = createCoverageMap();
+
+const loadCoverageData = (coverageDir) => {
+    const coverageFile = path.join(coverageDir, 'coverage-final.json');
+    const coverageData = JSON.parse(fs.readFileSync(coverageFile, 'utf8'));
+    return coverageData;
+};
+
+const mergeCoverageData = (coverageData) => {
+    coverageMap.merge(coverageData);
+};
+
+const generateReports = () => {
+    const reporter = createReporter();
+    reporter.addAll(['html', 'json', 'text-summary']);
+    reporter.write(coverageMap);
+};
+
+const jestCoverageData = loadCoverageData(coverageJestDir);
+const cucumberCoverageData = loadCoverageData(coverageCucumberDir);
+
+mergeCoverageData(jestCoverageData);
+mergeCoverageData(cucumberCoverageData);
+generateReports();
