819ad024179fda3a0b8a4af22df1d817da7fc40a
joeylam <joey.cm.lam@gmail.com>
2023-05-27 22:01:19 +0800

    reflector the feature test

--- test/test-fee/features/step_definitions/fee.steps.ts
+++ test/test-fee/features/step_definitions/fee.steps.ts
@@ -1,14 +1,11 @@
 import {Before, Given, setWorldConstructor, Then, When} from "@cucumber/cucumber";
-import FeeCalculator from "../../../../src/app/app-fee/feeCalculator";
+import FeeCalculator, {Transaction} from "../../../../src/app/app-fee/feeCalculator";
 import {expect} from 'chai';
 import {feeCustom} from "../../../support/world";
 import * as path from "path";
 
 let dataFile: any;
-let orderFundId;
-let orderTxnType;
-let orderTxnUnit;
-let orderTradeDate;
+let order: Transaction;
 let a: FeeCalculator;
 let feeAmount: number;
 Before(()=> {
@@ -20,25 +17,25 @@ Before(()=> {
 Given('the account current position file {string} and place {string} on {string} with {int} unit on {string}', (
     inDataFile: string, tradeType: string, fundId: string, sellUnit: number, orderDate: string) => {
     // a.dataFile = path.join('test/test-fee/test-data/', inDataFile);
-    // a.order.fundId = fundId;
-    // a.order.txnType = tradeType;
-    // a.order.txnUnit = sellUnit;
-    // a.order.tradeDate = orderDate;
+    order.fundId = fundId;
+    order.txnType = tradeType;
+    order.txnUnit = sellUnit;
+    order.tradeDate = orderDate;
     dataFile = path.join('test/test-fee/test-data/', inDataFile);
-    orderFundId = fundId;
-    orderTxnType = tradeType;
-    orderTxnUnit = sellUnit;
-    orderTradeDate = orderDate;
+    // orderFundId = fundId;
+    // orderTxnType = tradeType;
+    // orderTxnUnit = sellUnit;
+    // orderTradeDate = orderDate;
 });
 
 When('call the calculator',async () => {
     a = new FeeCalculator();
     // a.transactions = await a.feeCalculator.readTransactionsFromFile(dataFile);
 
-    if (orderTxnType === 'SELL') {
-        feeAmount = a.calculateFee(orderTradeDate, orderTradeDate, orderTxnUnit);
+    if (order.txnType === 'SELL') {
+        feeAmount = a.calculateFee(order.tradeDate, order.tradeDate, order.txnUnit);
     } else {
-        throw new Error(`Unsupported trade type: ${orderTxnType}`);
+        throw new Error(`Unsupported trade type: ${order.txnType}`);
     }
 });
 
