8a17cdc9be9b9ebb8572a4aa1d10e5ed8ce3ad6d
joeylam <joey.cm.lam@gmail.com>
2023-05-27 15:28:35 +0800

    fix cucumber test

--- test/test-fee/features/step_definitions/fee.steps.ts
+++ test/test-fee/features/step_definitions/fee.steps.ts
@@ -1,30 +1,43 @@
-import {Given, Then, When} from "@cucumber/cucumber";
-import FeeCalculator, { Transaction } from "../../../../src/app/app-fee/feeCalculator";
-import { expect } from 'chai';
+import {BeforeAll, Given, setWorldConstructor, Then, When} from "@cucumber/cucumber";
+import FeeCalculator, {Transaction} from "../../../../src/app/app-fee/feeCalculator";
+import {expect} from 'chai';
+import * as path from "path";
+import CustomWorld from "../../../support/world";
 
-let feeCalculator: FeeCalculator;
-let transactions: Transaction[];
-let sellUnits: number;
-let tradeType: string;
-let tradeDate: string;
-let feeAmount: number;
+class feeCustom{
+    dataFile: string;
+    feeCalculator: FeeCalculator;
+    transactions: Transaction[];
+    order: Transaction;
+    feeAmount: number;
+};
 
-Given('the account current position file "{string}" and place "{string}" {int} at "{string}"', (inDataFile: string, currentTradeType: string, sellUnit: number, currentTradeDate: string) => {
-    feeCalculator = new FeeCalculator();
-    transactions = feeCalculator.readTransactionsFromFile(inDataFile);
-    sellUnits = sellUnit;
-    tradeType = currentTradeType;
-    tradeDate = currentTradeDate;
+
+BeforeAll(async () => {
+    setWorldConstructor(feeCustom);
 });
 
-When('call the calculator', () => {
-    if (tradeType === 'SELL') {
-        feeAmount = feeCalculator.calculateFee(tradeDate, transactions[0].tradeDate, sellUnits);
+Given('the account current position file {string} and place {string} on {string} with {int} unit on {string}', (
+    a: feeCustom,
+    inDataFile: string, tradeType: string, fundId: string, sellUnit: number, orderDate: string) => {
+    a.feeCalculator = new FeeCalculator();
+    a.transactions = a.feeCalculator.readTransactionsFromFile(inDataFile);
+    a.order.fundId = fundId;
+    a.order.txnType = tradeType;
+    a.order.txnUnit = sellUnit;
+    a.order.tradeDate = orderDate;
+});
+
+When('call the calculator', (a: feeCustom) => {
+    a.transactions = a.feeCalculator.readTransactionsFromFile(a.dataFile);
+
+    if (a.order.txnType === 'SELL') {
+        a.feeAmount = a.feeCalculator.calculateFee(a.order.tradeDate, a.transactions[0].tradeDate, a.order.txnUnit);
     } else {
-        throw new Error(`Unsupported trade type: ${tradeType}`);
+        throw new Error(`Unsupported trade type: ${a.order.txnType}`);
     }
 });
 
-Then('total fee value is {int}', (expectedFeeAmount: number) => {
-    expect(feeAmount).to.equal(expectedFeeAmount);
+Then('total fee value is {int}', (a: feeCustom, expectedFeeAmount: number) => {
+    expect(a.feeAmount).to.equal(expectedFeeAmount);
 });
