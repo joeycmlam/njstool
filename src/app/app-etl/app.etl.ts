aed8737818e7b2b6aa7c27bf3c0bb4c81c7bc4c7
joeylam <joey.cm.lam@gmail.com>
2023-11-06 00:07:42 +0800

    formatting the file

--- src/app/app-etl/app.etl.ts
+++ src/app/app-etl/app.etl.ts
@@ -1,57 +1,58 @@
-import {ConfigHelper} from "../lib/configHelper";
-import {AppEtlConfig} from "./appEtlConfig";
-import {ETLProcesser, FileProcessorConfig} from "./etlProcesser";
-import PostgresUploader from "../lib/postgresUploader";
+import { ConfigHelper } from "../lib/configHelper";
 import ExcelReader from "../lib/excelReader";
 import Logger from "../lib/logger";
+import PostgresUploader from "../lib/postgresUploader";
 import { accountConfig } from "./accountConfig";
+import { AppEtlConfig } from "./appEtlConfig";
+import { ETLProcesser, FileProcessorConfig } from "./etlProcesser";
 import { holdingConfig } from "./holdingConfig";
 
 class EtlRunner {
-  private accountConfig: FileProcessorConfig;
-  private holdingConfig: FileProcessorConfig;
-  private config: AppEtlConfig;
-  private logger: Logger;
+    private accountConfig: FileProcessorConfig;
+    private holdingConfig: FileProcessorConfig;
+    private config: AppEtlConfig;
+    private logger: Logger;
 
-  constructor(dbConfig: AppEtlConfig, accountConfig: FileProcessorConfig, holdingConfig: FileProcessorConfig) {
-    this.accountConfig = accountConfig;
-    this.holdingConfig = holdingConfig;
-    this.logger = Logger.getInstance();
-    this.config = dbConfig;
-  }
+    constructor(dbConfig: AppEtlConfig, accountConfig: FileProcessorConfig, holdingConfig: FileProcessorConfig) {
+        this.accountConfig = accountConfig;
+        this.holdingConfig = holdingConfig;
+        this.logger = Logger.getInstance();
+        this.config = dbConfig;
+    }
 
-  async run() {
-    this.logger.info('initialization')
-    const accountReader = new ExcelReader(this.accountConfig.fileName);
-    const accountUploader = new PostgresUploader(this.config.database);
-    const accountProcessor = new ETLProcesser(this.accountConfig, accountUploader, accountReader);
+    async run() {
+        this.logger.info('initialization')
+        const accountReader = new ExcelReader(this.accountConfig.fileName);
+        const accountUploader = new PostgresUploader(this.config.database);
+        const accountProcessor = new ETLProcesser(this.accountConfig, accountUploader, accountReader);
 
-    const holdingReader = new ExcelReader(this.holdingConfig.fileName);
-    const holdingUploader = new PostgresUploader(this.config.database);
-    const holdingProcessor = new ETLProcesser(this.holdingConfig, holdingUploader, holdingReader);
+        const holdingReader = new ExcelReader(this.holdingConfig.fileName);
+        const holdingUploader = new PostgresUploader(this.config.database);
+        const holdingProcessor = new ETLProcesser(this.holdingConfig, holdingUploader, holdingReader);
 
-    this.logger.info('start upload account and holding');
-    await Promise.all([
-        accountProcessor.process(),
-        holdingProcessor.process()
-    ]);
+        this.logger.info('start upload account and holding');
+        await Promise.all([
+            accountProcessor.process(),
+            holdingProcessor.process()
+        ]);
 
-    this.logger.info('done');
-  }
+        this.logger.info('done');
+    }
 }
 
 (async () => {
 
-   const configFile = 'src/app/app-etl/config.etl.yaml';
-   const configHelper = new ConfigHelper(configFile);
-   await configHelper.load();
-   const config = configHelper.getConfig() as AppEtlConfig;
+    const configFile = 'src/app/app-etl/config.etl.yaml';
+    const configHelper = new ConfigHelper(configFile);
+    await configHelper.load();
+    const config = configHelper.getConfig() as AppEtlConfig;
 
-   const accConfig = accountConfig;
-   accConfig.fileName = config.dataFilePath.accountFile;
+    const accConfig = accountConfig;
+    accConfig.fileName = config.dataFilePath.accountFile;
 
-   const holdConfig = holdingConfig;
-   holdConfig.fileName = config.dataFilePath.holdingFile;
-  const runner = new EtlRunner(config, accountConfig, holdingConfig);
-  await runner.run();
+    const holdConfig = holdingConfig;
+    holdConfig.fileName = config.dataFilePath.holdingFile;
+
+    const runner = new EtlRunner(config, accountConfig, holdingConfig);
+    await runner.run();
 })();
