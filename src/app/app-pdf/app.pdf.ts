e2e464602f65cb72a8192bea6bab86a4ccb963d4
joeylam <joey.cm.lam@gmail.com>
2023-12-03 00:19:34 +0800

    externalize the info

--- src/app/app-pdf/app.pdf.ts
+++ src/app/app-pdf/app.pdf.ts
@@ -1,18 +1,21 @@
 import path from 'path';
 import { PdfComparator } from './PdfComparator';
 import { Logger } from '../lib/logger';
+import config from './config';
 
 async function main() {
 
   const logger = Logger.getLogger();
-  const folder1 = path.join(__dirname, 'data/a');
-  const folder2 = path.join(__dirname, 'data/b');
+  const folder1 = path.join(__dirname, config.folder1);
+  const folder2 = path.join(__dirname, config.folder2);
 
+  // Compare two folders with PDF files in folders
   logger.info('Comparing PDF files in folders %s and %s.', folder1, folder2);
-  // await PdfComparator.comparePdfFolders(folder1, folder2);
+  await PdfComparator.comparePdfFolders(folder1, folder2);
 
-  const file1 = path.join(__dirname, 'data/a/dummy.pdf');
-  const file2 = path.join(__dirname, 'data/b/dummy.pdf');
+  // Compare two PDF files
+  const file1 = path.join(__dirname, config.file1);
+  const file2 = path.join(__dirname, config.file2);
   const result = await PdfComparator.comparePdfFiles(file1, file2);
   logger.info('The files %s and %s are %s.', file1, file2, result ? 'identical' : 'different');
   logger.info('Done.');
---
+++ src/app/app-pdf/config.ts
@@ -0,0 +1,6 @@
+export default {
+    folder1: 'data/a',
+    folder2: 'data/b',
+    file1: 'data/a/sample.pdf',
+    file2: 'data/b/sample.pdf'
+  };
