3a82061b3a8a8b9506eb375cf266e33fd608eac5
joeylam <joey.cm.lam@gmail.com>
2023-12-02 23:38:15 +0800

    improve to consider file in 1 folder only

--- src/app/app-pdf/PdfComparator.ts
+++ src/app/app-pdf/PdfComparator.ts
@@ -1,11 +1,36 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as pdf from 'pdf-parse';
 
 export class PdfComparator {
-    static async comparePdfFiles(file1: string, file2: string): Promise<boolean> {
-        const data1 = await pdf.default(fs.readFileSync(file1));
-        const data2 = await pdf.default(fs.readFileSync(file2));
+  static async comparePdfFiles(file1: string, file2: string): Promise<boolean> {
+    const data1 = await pdf.default(fs.readFileSync(file1));
+    const data2 = await pdf.default(fs.readFileSync(file2));
 
-        return data1.text === data2.text;
+    return data1.text === data2.text;
+  }
+
+  static async comparePdfFolders(folder1: string, folder2: string): Promise<void> {
+    const files1 = fs.readdirSync(folder1);
+    const files2 = fs.readdirSync(folder2);
+  
+    for (const file of files1) {
+      if (files2.includes(file)) {
+        const file1Path = path.join(folder1, file);
+        const file2Path = path.join(folder2, file);
+  
+        const areEqual = await PdfComparator.comparePdfFiles(file1Path, file2Path);
+  
+        console.info(`The files ${file} are ${areEqual ? 'identical' : 'different'}.`);
+      } else {
+        console.info(`The file ${file} exists in ${folder1} but not in ${folder2}.`);
+      }
     }
+  
+    for (const file of files2) {
+      if (!files1.includes(file)) {
+        console.info(`The file ${file} exists in ${folder2} but not in ${folder1}.`);
+      }
+    }
+  }
 }
--- src/app/app-pdf/app.pdf.ts
+++ src/app/app-pdf/app.pdf.ts
@@ -3,24 +3,17 @@ import { PdfComparator } from './PdfComparator';
 import * as log4js from 'log4js';
 
 log4js.configure({
-    appenders: { out: { type: 'stdout' } },
-    categories: { default: { appenders: ['out'], level: 'info' } }
-  });
+  appenders: { out: { type: 'stdout' } },
+  categories: { default: { appenders: ['out'], level: 'info' } }
+});
 
 const logger = log4js.getLogger();
 
 async function main() {
-    const file1 = path.join(__dirname, 'data/a/sample.pdf');
-    const file2 = path.join(__dirname, 'data/b/sample.pdf');
-  
+  const folder1 = path.join(__dirname, 'data/a');
+  const folder2 = path.join(__dirname, 'data/b');
 
-  const areEqual = await PdfComparator.comparePdfFiles(file1, file2);
-
-  if (areEqual) {
-    logger.info('The files are identical.');
-  } else {
-    logger.info('The files are different.');
-  }
+  await PdfComparator.comparePdfFolders(folder1, folder2);
 }
 
 main().catch(err => logger.error(err));
