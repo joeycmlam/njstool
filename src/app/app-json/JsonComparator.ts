6093986f323765342ebc79e2e0ca0a8b6d38b8ec
joeylam <joey.cm.lam@gmail.com>
2023-12-03 02:22:17 +0800

    reflect to enhance the code

--- src/app/app-json/JsonComparator.ts
+++ src/app/app-json/JsonComparator.ts
@@ -19,41 +19,56 @@ export class JsonComparator {
     }
   }
 
-  compare(obj1: { [key: string]: any }, obj2: { [key: string]: any }, fieldName: string = ''): boolean {
+  compareValues(value1: any, value2: any, fileName: string, fieldName: string): boolean {
+    const isEqual = value1 === value2;
+    this.writeResultDetails(fileName, fieldName, isEqual, value1, value2);
+    return isEqual;
+  }
+
+  compareObjects(obj1: { [key: string]: any }, obj2: { [key: string]: any }, fileName: string, fieldName: string): boolean {
     let isEqual = true;
 
-    // If both are arrays, sort them and compare each item
-    if (_.isArray(obj1) && _.isArray(obj2)) {
-      const sortedObj1 = [...obj1].sort();
-      const sortedObj2 = [...obj2].sort();
-      for (let i = 0; i < Math.max(sortedObj1.length, sortedObj2.length); i++) {
-        const item1 = sortedObj1[i];
-        const item2 = sortedObj2[i];
-        const itemIsEqual = item1 === item2;
-        fs.appendFileSync(this.resultsFile, `${fieldName}[${i + 1}]|${itemIsEqual ? ComparisonResult.Match : ComparisonResult.Unmatch}|${JSON.stringify(item1)}|${JSON.stringify(item2)}\n`);
-        if (!itemIsEqual) {
-          isEqual = false;
-        }
+    // ...
+    for (const key in obj1) {
+      if (!this.compare(obj1[key], obj2[key], key, fileName)) {
+        isEqual = false;
       }
-    }
-    // If both are objects, compare their properties
-    else if (_.isObject(obj1) && _.isObject(obj2)) {
-      for (const key in obj1) {
-        if (!this.compare(obj1[key], obj2[key], key)) {
-          isEqual = false;
-        }
-      }
-    }
-    // Otherwise, just compare the values
-    else {
-      isEqual = obj1 === obj2;
-      fs.appendFileSync(this.resultsFile, `${fieldName}|${isEqual ? ComparisonResult.Match : ComparisonResult.Unmatch}|${JSON.stringify(obj1)}|${JSON.stringify(obj2)}\n`);
     }
 
     return isEqual;
   }
 
+  compareArrays(obj1: any[], obj2: any[], fileName: string, fieldName: string): boolean {
+    let isEqual = true;
+
+    // ...
+    const sortedObj1 = [...obj1].sort();
+    const sortedObj2 = [...obj2].sort();
+    for (let i = 0; i < Math.max(sortedObj1.length, sortedObj2.length); i++) {
+      const item1 = sortedObj1[i];
+      const item2 = sortedObj2[i];
+      const itemIsEqual = item1 === item2;
+      this.writeResultDetails(fileName, `${fieldName}[${i + 1}]`, itemIsEqual, item1, item2);
+      if (!itemIsEqual) {
+        isEqual = false;
+      }
+    }
+    return isEqual;
+  }
+
+  compare(obj1: { [key: string]: any }, obj2: { [key: string]: any },fieldName: string = '', fileName: string ): boolean {
+    if (_.isArray(obj1) && _.isArray(obj2)) {
+      return this.compareArrays(obj1, obj2, fileName, fieldName);
+    } else if (_.isObject(obj1) && _.isObject(obj2)) {
+      return this.compareObjects(obj1, obj2, fileName, fieldName);
+    } else {
+      return this.compareValues(obj1, obj2, fileName, fieldName);
+    }
+  }
+
   compareFolders(folder1: string, folder2: string): void {
+    this.writeHeader();
+
     const files1 = fs.readdirSync(folder1);
     const files2 = fs.readdirSync(folder2);
 
@@ -62,8 +77,18 @@ export class JsonComparator {
       if (file2) {
         const json1 = JSON.parse(fs.readFileSync(path.join(folder1, file1), 'utf-8'));
         const json2 = JSON.parse(fs.readFileSync(path.join(folder2, file2), 'utf-8'));
-        this.compare(json1, json2, path.basename(file1, '.json'));
+        this.compare(json1, json2, path.basename(file1, '.json'), file1);
       }
     }
   }
+
+
+  writeHeader(): void {
+    fs.writeFileSync(this.resultsFile, 'FileName|FieldName|ComparisonResult|Value1|Value2\n');
+  }
+
+  writeResultDetails(fileName: string, fieldName: string, isEqual: boolean, value1: any, value2: any): void {
+    fs.appendFileSync(this.resultsFile, `${fileName}|${fieldName}|${isEqual ? ComparisonResult.Match : ComparisonResult.Unmatch}|${JSON.stringify(value1)}|${JSON.stringify(value2)}\n`);
+  }
+
 }
