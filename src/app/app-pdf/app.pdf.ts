c48acebd21b1863d9deed7f0b344a7208964534a
joeylam <joey.cm.lam@gmail.com>
2023-12-03 00:14:38 +0800

    small fix

--- src/app/app-pdf/app.pdf.ts
+++ src/app/app-pdf/app.pdf.ts
@@ -9,7 +9,12 @@ async function main() {
   const folder2 = path.join(__dirname, 'data/b');
 
   logger.info('Comparing PDF files in folders %s and %s.', folder1, folder2);
-  await PdfComparator.comparePdfFolders(folder1, folder2);
+  // await PdfComparator.comparePdfFolders(folder1, folder2);
+
+  const file1 = path.join(__dirname, 'data/a/dummy.pdf');
+  const file2 = path.join(__dirname, 'data/b/dummy.pdf');
+  const result = await PdfComparator.comparePdfFiles(file1, file2);
+  logger.info('The files %s and %s are %s.', file1, file2, result ? 'identical' : 'different');
   logger.info('Done.');
 }
 
