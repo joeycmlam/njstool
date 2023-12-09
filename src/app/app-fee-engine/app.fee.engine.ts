cb7e544e1c545bbdd9489b74d8a1d427ae88d813
joeylam <joey.cm.lam@gmail.com>
2023-12-10 00:19:41 +0800

    reflector

--- src/app/app-fee-engine/app.fee.engine.ts
+++ src/app/app-fee-engine/app.fee.engine.ts
@@ -1,30 +1,32 @@
-import DSCCalculator, {DSCSchedule} from "./DSCCalculator";
-import {InvestmentTransaction} from "./typeFeeEngine";
-import {enumTnxType} from "./enumFeeEngine";
+import DSCCalculator, { DSCSchedule } from "./DSCCalculator";
+import { InvestmentTransaction } from "./typeFeeEngine";
+import { enumTnxType } from "./enumFeeEngine";
 import FIFOHoldingPeriodCalculator from "./FIFOHoldingPeriodCalculator";
-import Logger from "../lib/logger";
+import { Logger } from "../lib/logger";
+import { Container, injectable } from "inversify";
 
+@injectable()
 class app {
+    private logger = Logger.getLogger();
 
+    constructor() {
+    }
 
-    static async  process() {
-        logger.info('process.start');
-// Example usage:
+    async run() {
+        this.logger.info('process.start');
+        // Example usage:
         const transactions: InvestmentTransaction[] = [
-            {referenceId: 'T00001', txnDate: new Date('2019-01-01'), units: 100, transactionType: enumTnxType.SUBSCRIPTION },
-            {referenceId: 'T00002', txnDate: new Date('2020-06-01'), units: 50, transactionType: enumTnxType.SUBSCRIPTION},
-            {referenceId: 'T00003', txnDate: new Date('2021-07-01'), units: 100, transactionType: enumTnxType.SUBSCRIPTION},
-            {referenceId: 'T00004', txnDate: new Date('2022-01-01'), units: 120, transactionType: enumTnxType.REDEMPTION },
+            { referenceId: 'T00001', txnDate: new Date('2019-01-01'), units: 100, transactionType: enumTnxType.SUBSCRIPTION },
+            { referenceId: 'T00002', txnDate: new Date('2020-06-01'), units: 50, transactionType: enumTnxType.SUBSCRIPTION },
+            { referenceId: 'T00003', txnDate: new Date('2021-07-01'), units: 100, transactionType: enumTnxType.SUBSCRIPTION },
+            { referenceId: 'T00004', txnDate: new Date('2022-01-01'), units: 120, transactionType: enumTnxType.REDEMPTION },
         ];
 
         const referenceDate = new Date('2023-07-08'); // The date to calculate holding periods
         const fifoHoldingPeriodCalculator = new FIFOHoldingPeriodCalculator(transactions);
         const holdingPeriods = fifoHoldingPeriodCalculator.calculateHoldingPeriods(referenceDate, 40);
 
-        logger.info(`Holding periods:{$holdingPeriods}`);
-
-
-
+        this.logger.info(`Holding periods:${JSON.stringify(holdingPeriods)}`);
 
         const dscSchedule: DSCSchedule = [
             { year: 1, percentage: 0.05 },
@@ -36,19 +38,14 @@ class app {
         const dscCalculator = new DSCCalculator(dscSchedule);
 
         const dscTransactions = dscCalculator.calculateDSC(holdingPeriods);
-        logger.info(`The Deferred Sales Charge: JSON.stringify(dscTransactions)`);
+        this.logger.info(`The Deferred Sales Charge: ${JSON.stringify(dscTransactions)})`);
 
-        logger.info('process.end');
+        this.logger.info('process.end');
     }
 }
 
-const logger = Logger.getInstance();
 
-(async () => {
+const container = new Container();
 
-    logger.info('start');
-
-    app.process();
-    logger.info('end');
-})();
-
+const main = container.resolve(app);
+main.run();
