d9f93a968291b31cfb5dd737a7727033137fe7ca
joeylam <joey.cm.lam@gmail.com>
2024-03-24 22:43:16 +0800

    fix the test

--- src/app/app-playloader/test/features/step_definitions/playload.steps.ts
+++ src/app/app-playloader/test/features/step_definitions/playload.steps.ts
@@ -1,12 +1,13 @@
-import { Given, When, Then } from '@cucumber/cucumber';
+import { Given, Then, When } from '@cucumber/cucumber';
 import assert from 'assert';
 import fs from 'fs';
-import ExcelReader from '../../../ExcelReader';
-import DataTransformer from '../../../DataTransformer';
 import path from 'path';
+import DataTransformer from '../../../DataTransformer';
+import ExcelReader from '../../../ExcelReader';
 
-let excelFile: string, jsonOutput: any, expectedJson: any;
-let recordName: string, actualJsonData: any;
+let excelFile: string;
+let actualJsonData: any;
+let actualResult: {recordName: string, jsonData: any};
 
 Given('I have an Excel file {string}', function (givenExcelFile: string) {
   excelFile = givenExcelFile;
@@ -19,14 +20,14 @@ When('I convert the Excel file to JSON', async function () {
   const workbook = await excelReader.read(fileName);
   const worksheet = workbook.worksheets[0];
   const dataTransformer = new DataTransformer();
-  let {recordName, actualJsonData } = dataTransformer.transform(worksheet, 3);
-
+  let actualResult = await dataTransformer.transform(worksheet, 3);
 });
 
 Then('the JSON output should match the expected JSON file {string}', function (expectedJsonFile:string) {
   const fileName: string = path.join(__dirname, expectedJsonFile);
   const expectedJson = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  assert.deepStrictEqual(actualJsonData, expectedJson);
+  console.log('actualResult', JSON.stringify(actualResult));
+  assert.deepStrictEqual(actualResult.jsonData, expectedJson);
 });
 
 
