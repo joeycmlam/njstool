43145e48980b8b80922a9e0bf4a9dd9b3574eb75
joeylam <joey.cm.lam@gmail.com>
2024-03-25 10:46:02 +0800

    fix cucumber test

--- src/app/app-playloader/test/features/step_definitions/playload.steps.ts
+++ src/app/app-playloader/test/features/step_definitions/playload.steps.ts
@@ -7,6 +7,23 @@ import ExcelReader from '../../../ExcelReader';
 
 let excelFile: string;
 let actualResult: any;
+let config = {
+  "source": {
+      "inputPath": "/Users/joeylam/repo/njs/njstool/src/app/app-playloader/data",
+      "inputFile": "sample.xlsx"
+  },
+  "dataformat": {
+      "sheetName": "Sheet1",
+      "startRow": 1,
+      "fieldCol": 1,
+      "typeCol": 2,
+      "dataStartCol": 3,
+      "dataEndCol": 4
+  },
+  "output": {
+      "outputPath": "/Users/joeylam/repo/njs/njstool/src/app/app-playloader/output"
+  }    
+}
 
 Given('I have an Excel file {string}', function (givenExcelFile: string) {
   excelFile = givenExcelFile;
@@ -19,7 +36,7 @@ When('I convert the Excel file to JSON', async function () {
   const workbook = await excelReader.read(fileName);
   const worksheet = workbook.worksheets[0];
   const dataTransformer = new DataTransformer();
-  actualResult = await dataTransformer.transform(worksheet, 3);
+  actualResult = await dataTransformer.transform(worksheet, 3, config.dataformat);
 });
 
 Then('the JSON output should match the expected JSON file {string}', function (expectedJsonFile:string) {
