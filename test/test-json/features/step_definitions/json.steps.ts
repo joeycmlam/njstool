28088ae1d6a00c4aa0e4f616e62716a8108337a3
joeylam <joey.cm.lam@gmail.com>
2024-04-06 00:29:01 +0800

    resturcture the test folders

--- test/test-json/features/step_definitions/json.steps.ts
+++ test/test-json/features/step_definitions/json.steps.ts
@@ -5,24 +5,34 @@ import * as assert from 'assert';
 import JsonHelper from "../../../../src/app/app-json/jsonHelper";
 import excelHelper from "../../../../src/app/lib/excelHelper";
 
+const config = {
+    "dataPath": "/Users/joeylam/repo/njs/njstool/test/test-json/data",
+    "outputPath": "/Users/joeylam/repo/njs/njstool/test/test-json/output",
+    "expectedPath": "/Users/joeylam/repo/njs/njstool/test/test-json/features/expected"
+};
 
-Given('I have a JSON file at {string} {string}', function (this: CustomWorld, datapath: string, fileName: string) {
-    this.dataPath = datapath;
-    this.jsonFile = path.join(this.dataPath, fileName);
+let jsonFileName: string;
+let actualFileName: string;
+let expectedFileName: string;
+
+Given('I have a JSON file at {string} {string}', function (datapath: string, fileName: string) {
+    // this.dataPath = config.dataPath;
+    jsonFileName = path.join(config.dataPath, fileName);
 });
 
-When('I generate the Excel file {string}', async function (this: CustomWorld, actualFile: string) {
+When('I generate the Excel file {string}', async function (actualFile: string) {
 
-    this.actualFile = path.join(this.dataPath, actualFile);
+    actualFileName = path.join(config.outputPath, actualFile);
+    console.log('actualFile: ', actualFileName);
     const a: JsonHelper = new JsonHelper();
-    await a.processJsonfile(this.jsonFile);
-    await a.write2excel(this.actualFile, 'sheet1');
+    await a.processJsonfile(jsonFileName);
+    await a.write2excel(actualFileName, 'sheet1');
 });
 
-Then('the generated Excel file should match the expected file {string}', async function (this: CustomWorld, expectedFile: string) {
-    this.expectedFile = path.join(this.dataPath, expectedFile);
-    const expectedContent = await excelHelper.readWorkbookContent(this.expectedFile, 'sheet1');
-    const generatedContent = await excelHelper.readWorkbookContent(this.actualFile, 'sheet1');
+Then('the generated Excel file should match the expected file {string}', async function (expectedFile: string) {
+    const expectedFileName = path.join(this.dataPath, expectedFile);
+    const expectedContent = await excelHelper.readWorkbookContent(expectedFileName, 'sheet1');
+    const generatedContent = await excelHelper.readWorkbookContent(actualFileName, 'sheet1');
 
     assert.deepStrictEqual(generatedContent, expectedContent, 'Generated Excel file does not match the expected file');
 });
