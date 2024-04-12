9d5d0add9018eccd5044fc7bbc170a953573db71
joeylam <joey.cm.lam@gmail.com>
2024-04-12 20:34:58 +0800

    code reflector to use same excelHelper

--- src/app/app-playloader/app.playloader.ts
+++ src/app/app-playloader/app.playloader.ts
@@ -2,7 +2,7 @@ import Logger from '../lib/logger';
 import path from 'path';
 import ExcelToJsonConverter from './ExcelToJsonConverter';
 import JsonFileWriter from './JsonFileWriter';
-import ExcelReader from './ExcelReader';
+import excelHelper from '../lib/excelHelper';
 import { IDataTransformer } from './IDataTransformer';
 import * as fs from 'fs';
 import minimist from 'minimist';
@@ -26,7 +26,7 @@ class Main {
 
         const inFile = path.join(config.source.inputPath, config.source.inputFile);
         const outFilePath = config.output.outputPath;
-        const excelReader = new ExcelReader(); // Fix: Pass the required arguments to the constructor
+        const excelReader = new excelHelper(); // Fix: Pass the required arguments to the constructor
         const jsonFileWriter = new JsonFileWriter(); // Fix: Pass the required arguments to the constructor
         const converter = new ExcelToJsonConverter(config, excelReader, this.dataTransformer, jsonFileWriter); // Fix: Pass the required arguments to the constructor
         const arr: any = await converter.convert(inFile, outFilePath);
