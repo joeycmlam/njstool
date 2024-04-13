91daf3b33406416f9364eb4c96bd59fd30fde697
joeylam <joey.cm.lam@gmail.com>
2024-04-13 16:30:14 +0800

    reflector the log

--- src/app/lib/logger.ts
+++ src/app/lib/logger.ts
@@ -1,51 +1,48 @@
 import * as log4js from 'log4js';
 import path from 'path';
-import fs from 'fs';
-import yaml from 'js-yaml';
 import 'reflect-metadata';
 
+export interface LoggerConfig {
+  level?: string;
+  path?: string;
+  filename?: string;
+}
 
 export default class Logger {
-  private static instance: Logger;
-  private static config: { filename?: string, level?: string };
+  private static config: LoggerConfig = { level: 'info' };
 
   static getLogger(): log4js.Logger {
     return log4js.getLogger();
   }
 
-  static {
-    let config: { filename?: string, level?: string };
-    const configFile = path.resolve(require.main!.path!, './config.logger.yaml');
-
-    try {
-      const configData = fs.readFileSync(configFile, 'utf-8');
-      config = yaml.load(configData) as { filename?: string, level?: string };
-    } catch (err) {
-      // Default configuration
-      config = {
-        filename: 'app',
-        level: 'info'
-      };
-    }
-
-    Logger.instance = new Logger(config);
-  }
-  
-  private constructor(config: { filename?: string, level?: string }) {
-    const logFilePattern = 'yyyyMMdd.log';
+  static configure(config: LoggerConfig) {
     Logger.config = config;
+    Logger.setConfigure();
+  }
 
-    const fileName = Logger.config.filename || 'app';
-    const level = Logger.config.level || 'info';
+  static {
+    Logger.setConfigure();
+  }
 
+  private static setConfigure() {
+
+    const appenders: { [key: string]: any } = {
+      console: { type: 'console' },
+    };
+
+    if (Logger.config.filename) {
+      const filePath = Logger.config.path || require.main!.path!;
+      const fileFullName = path.join(filePath, Logger.config.filename);
+      appenders['fileAppender'] = { type: 'dateFile', filename: fileFullName, pattern: 'yyyyMMdd.log', alwaysIncludePattern: true, keepFileExt: true };
+    }
+  
+  
     log4js.configure({
-      appenders: {
-        fileAppender: { type: 'dateFile', filename: fileName, pattern: logFilePattern, alwaysIncludePattern: true, keepFileExt: true },
-        console: { type: 'console' },
-      },
+      appenders: appenders,
       categories: {
-        default: { appenders: ['fileAppender', 'console'], level: level },
+        default: { appenders: Object.keys(appenders), level: Logger.config.level || 'info' },
       },
     });
   }
+
 }
