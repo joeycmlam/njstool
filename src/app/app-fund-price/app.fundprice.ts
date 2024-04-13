5639a2a9d9ada1c8c86580a9e84423f4a42ccc7e
joeylam <joey.cm.lam@gmail.com>
2024-04-13 18:04:20 +0800

    fundprice service skeleton

---
+++ src/app/app-fund-price/app.fundprice.ts
@@ -0,0 +1,38 @@
+import { Container, injectable } from 'inversify';
+import Logger from '../lib/logger';
+import minimist from 'minimist';
+import path from 'path';
+import fs from 'fs';
+
+@injectable()
+class App {
+    private logger = Logger.getLogger();
+
+    constructor() {
+
+    }
+
+    private init(): void {
+        // Load configuration
+        // Get the config file path from the command line arguments
+        const args = minimist(process.argv.slice(2));
+        const configFile = args.config || path.join(__dirname, 'config.json');
+        const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+        Logger.configure(config.logger);
+
+    }
+
+    public async run() {
+
+        this.init();
+        this.logger.info('Start');
+
+        this.logger.info('End');
+    }
+
+
+}
+
+const container = new Container();
+const app = container.resolve(App);
+app.run();
