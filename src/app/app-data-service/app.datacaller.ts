cfb9b80ad13fe2bafeb7aadc0ef7864c53f2bddf
joeylam <joey.cm.lam@gmail.com>
2024-04-14 01:03:25 +0800

    enhance

--- src/app/app-data-service/app.datacaller.ts
+++ src/app/app-data-service/app.datacaller.ts
@@ -1,26 +1,39 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import Logger from '../lib/logger';
 
-async function getDataFromService() {
-  const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache()
-  });
-
-  const GET_ACCOUNTS = gql`
-    query GetAccounts {
-      accounts {
-        account_cd
-        account_nm
+class AppClient {
+  private logger = Logger.getLogger();
+  public constructor () {
+  }
+  
+  private async getDataFromService() {
+    const client = new ApolloClient({
+      uri: 'http://localhost:4000/graphql',
+      cache: new InMemoryCache()
+    });
+  
+    const GET_ACCOUNTS = gql`
+      query GetAccounts {
+        accounts {
+          account_cd
+          account_nm
+        }
       }
+    `;
+  
+    try {
+      const { data } = await client.query({ query: GET_ACCOUNTS });
+      this.logger.log(data.accounts);
+    } catch (error) {
+      this.logger.error('Error:', error);
     }
-  `;
+  }
 
-  try {
-    const { data } = await client.query({ query: GET_ACCOUNTS });
-    console.log(data.accounts);
-  } catch (error) {
-    console.error('Error:', error);
+  public run() {
+    this.getDataFromService();
   }
 }
 
-getDataFromService();
+
+const app = new AppClient();
+app.run();
