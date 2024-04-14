ab113ef253b85aff597133830005d59579639e26
joeylam <joey.cm.lam@gmail.com>
2024-04-14 18:04:02 +0800

    reflector

--- src/app/app-data-service/app.datacaller.ts
+++ src/app/app-data-service/app.datacaller.ts
@@ -1,39 +1,70 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import Logger from '../lib/logger';
+import minimist from 'minimist';
+import path from 'path';
+import fs from 'fs';
+import { promisify } from 'util';
 
 class AppClient {
   private logger = Logger.getLogger();
-  public constructor () {
+
+  private config: any = {};
+
+  public constructor() {
   }
-  
-  private async getDataFromService() {
-    const client = new ApolloClient({
-      uri: 'http://localhost:4000/graphql',
-      cache: new InMemoryCache()
-    });
-  
-    const GET_ACCOUNTS = gql`
-      query GetAccounts {
-        accounts {
-          account_cd
-          account_nm
-        }
-      }
-    `;
-  
-    try {
-      const { data } = await client.query({ query: GET_ACCOUNTS });
-      this.logger.log(data.accounts);
-    } catch (error) {
-      this.logger.error('Error:', error);
-    }
+
+  private initConfig(): void {
+    // Load configuration
+    // Get the config file path from the command line arguments
+    const args = minimist(process.argv.slice(2));
+    const configFile = args.config || path.join(__dirname, 'config.json');
+    this.config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
   }
 
   public run() {
     this.getDataFromService();
   }
+
+  private async getDataFromService() {
+
+    this.initConfig();
+
+    const client = new ApolloClient({
+      uri: this.config.url,
+      cache: new InMemoryCache()
+    });
+
+    let offset = 0;
+    const limit = 10;
+
+    while (true) {
+
+      const GET_ACCOUNTS = gql`
+      query GetAccounts {
+        accounts (limit: $limit, offSet: $offset) {
+          account_cd
+          account_nm
+        }
+      }
+    `;
+
+      try {
+        const { data } = await client.query({ query: GET_ACCOUNTS, variables: { limit, offset } });
+        this.logger.log(data.accounts);
+      } catch (error) {
+        this.logger.error('Error:', error);
+      }
+
+      offset += limit;
+
+      this.logger.log('records:', offset);
+      // await promisify(setTimeout)(1000);
+    }
+  }
+
 }
 
 
 const app = new AppClient();
 app.run();
+
