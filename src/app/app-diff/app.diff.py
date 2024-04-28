acdd4cfbb1c87d86f598c1ec6594daff09fbda46
joeylam <joey.cm.lam@gmail.com>
2024-04-28 23:56:36 +0800

    reflector

--- src/app/app-diff/app.diff.py
+++ src/app/app-diff/app.diff.py
@@ -1,6 +1,9 @@
 import argparse
 import configReader
+import logging
+import os
 from FileComparator2 import FileComparator
+from datetime import datetime
 
 class App:
     def __init__(self, config_path):
@@ -8,8 +11,17 @@ class App:
         self.config = self.config_reader.read_config()
         self.comparator = FileComparator(self.config)
 
+        # Set up logging
+        log_filename = f"{self.config['log']['filename']}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+        logging.basicConfig(filename=os.path.join(self.config['log']['path'], log_filename), 
+                            level=self.config['log']['level'].upper(), 
+                            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
+
     def run(self):
+        self.logger.info('Starting file comparison')
         self.comparator.compare_files()
+        self.logger.info('File comparison completed')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compare files based on a configuration file.')
