ceaef586bc27da51c7ae9b363b2c148929ff801d
joeylam <joey.cm.lam@gmail.com>
2024-04-28 23:51:26 +0800

    reflector

--- src/app/app-diff/app.diff.py
+++ src/app/app-diff/app.diff.py
@@ -2,13 +2,19 @@ import argparse
 import configReader
 from FileComparator2 import FileComparator
 
+class App:
+    def __init__(self, config_path):
+        self.config_reader = configReader.ConfigReader(config_path)
+        self.config = self.config_reader.read_config()
+        self.comparator = FileComparator(self.config)
+
+    def run(self):
+        self.comparator.compare_files()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compare files based on a configuration file.')
     parser.add_argument('--config', type=str, required=True, help='Path to the configuration file.')
     args = parser.parse_args()
 
-    config_reader = configReader.ConfigReader(args.config)
-    config = config_reader.read_config()
-
-    comparator = FileComparator(config)
-    comparator.compare_files()
+    app = App(args.config)
+    app.run()
