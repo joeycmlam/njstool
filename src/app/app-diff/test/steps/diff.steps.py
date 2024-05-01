e40852c34e9307e28c784bbbf965345b3c4499c8
joeylam <joey.cm.lam@gmail.com>
2024-05-01 22:59:50 +0800

    enhance cucumber test.

--- src/app/app-diff/test/steps/diff.steps.py
+++ src/app/app-diff/test/steps/diff.steps.py
@@ -1,28 +1,36 @@
 from behave import given, when, then
-from diff import FileComparator
-import pandas as pd
-import json
+from FileComparator2 import FileComparator
+import logging
 
-@given('I have two files "{file1}" and "{file2}" with config "{config}"')
-def step_given(context, file1, file2, config):
-    context.file1 = file1
-    context.file2 = file2
-    with open(config, 'r') as f:
-        context.config = json.load(f)
+@given('I have two files "{file}" with key "{key}"')
+def step_given_file_with_key(context, file, key):
+    context.file = file
+    context.key = key
 
 @when('I compare the files using FileComparator')
-def step_when(context):
-    context.config['files'] = [context.file1, context.file2]
-    comparator = FileComparator(context.config)
-    comparator.compare()
-    context.output_file = context.config['out']['filename']
+def step_when_compare_files(context):
+    config = {
+        "log": {
+            "level": "info",
+            "path": "logs",
+            "filename": "app.log"
+        },
+        "out": {
+            "path": "/Users/joeylam/repo/njs/njstool/src/app/app-diff/output",
+            "filename": "results.xlsx"
+        },
+        "path1": "/Users/joeylam/repo/njs/njstool/src/app/app-diff/test/data/data-1",
+        "path2": "/Users/joeylam/repo/njs/njstool/src/app/app-diff/test/data/data-2",
+        'files': [
+            {'filename': context.file, 'key': context.key},
+            {'filename': context.file, 'key': context.key}
+        ],
+        "rounding_threshold": 0.001
+    }
+    logger = logging.getLogger(__name__)
+    comparator = FileComparator(config, logger)
+    context.result = comparator.compare_files()
 
-@then('the total number of matching rows should be {matches:d}')
-def step_then_matches(context, matches):
-    df = pd.read_excel(context.output_file, sheet_name='Summary')
-    assert df['Number of Matches'].sum() == matches
-
-@then('the total number of mismatching rows should be {mismatches:d}')
-def step_then_mismatches(context, mismatches):
-    df = pd.read_excel(context.output_file, sheet_name='Summary')
-    assert df['Number of Mismatches'].sum() == mismatches
+@then('I expect the files result')
+def step_then_files_match(context):
+    assert context.result == [], "Files do not match"
