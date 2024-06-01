0409eff77ffcaa1e848913bc1751398655152cfe
joeylam <joey.cm.lam@gmail.com>
2024-06-01 23:23:56 +0800

    OpenAI sample

--- src/app/app-invest-advisor-v2/aiAdvisor.py
+++ src/app/app-invest-advisor-v2/aiAdvisor.py
@@ -2,7 +2,6 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-
 def make_connection():
     load_dotenv('/Users/joeylam/repo/njs/njstool/env/.env.local')
 
@@ -10,27 +9,17 @@ def make_connection():
     openai_api_key = os.environ.get('OPENAI_API_KEY')
 
     client = OpenAI(api_key=openai_api_key)
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "Say this is a test",
-            }
-        ],
-        model="gpt-3.5-turbo",
-    )
 
     return client
 
 def get_answer_from_chatgpt(question, context):
     client = make_connection()
-    response = client.ChatCompletion.create(
-        model="text-davinci-003",
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": context},
             {"role": "user", "content": question}
-        ],
-        max_tokens=150
+        ]
     )
-    return response['choices'][0]['message']['content'].strip()
+    return response
