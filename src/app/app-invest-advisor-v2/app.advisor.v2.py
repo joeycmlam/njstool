2b1825997e19c515187e66971099cdbea406367c
joeylam <joey.cm.lam@gmail.com>
2024-06-02 22:17:00 +0800

    enhancement to keep local topic to improve the performance

--- src/app/app-invest-advisor-v2/app.advisor.v2.py
+++ src/app/app-invest-advisor-v2/app.advisor.v2.py
@@ -1,7 +1,8 @@
 import json
-from sourceData import fetch_url_content_with_subpages, extract_datasource_url, fetch_url_content
+from contentManager import URLContentManager
 from aiAdvisor import get_answer_from_chatgpt
-from advisor import get_answer_nlp
+
+
 
 def main():
     # Load JSON data from a file
@@ -11,27 +12,18 @@ def main():
     # Extract the URL list from the JSON data
     urls = data['URL_LIST']
 
-    # Create a dictionary to store URL and its extracted content
-    url_contents = {}
-    for url in urls:
-        html_content = fetch_url_content(url)
-        if html_content:
-            text_content = extract_datasource_url(html_content)
-            url_contents[url] = text_content
-        else:
-            url_contents[url] = "Content could not be fetched"
+    # Create a URLContentManager and load or fetch content
+    url_content_manager = URLContentManager(urls)
+    url_content_manager.load_or_fetch_content()
 
     # Concatenate all the text contents from all URLs
-    all_contents = " ".join(url_contents.values())
+    all_contents = url_content_manager.get_all_contents()
 
     while True:
         question = input("What is your question? (Type 'bye' to exit) ")
         if question.lower() == 'bye':
             break
-        # Use all_contents as the context for the question
-        # answer = get_answer_from_chatgpt(question, all_contents)
-        # print(answer.choices[0].message.content)
-        answer = get_answer_nlp(question, all_contents)
+        answer = get_answer_from_chatgpt(question, all_contents)
         print(f"Answer: {answer}")
 
 if __name__ == "__main__":
