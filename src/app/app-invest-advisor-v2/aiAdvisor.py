bfb1da32c131db2e211652dcb02a42da34570242
joeylam <joey.cm.lam@gmail.com>
2024-06-08 18:22:01 +0800

    reflector

--- src/app/app-invest-advisor-v2/aiAdvisor.py
+++ src/app/app-invest-advisor-v2/aiAdvisor.py
@@ -1,34 +1,31 @@
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
-import json
 
-def make_connection():
-    # Load JSON data from a file
-    with open('config/config.json') as f:
-        data = json.load(f)
+class AIAdvisor:
+    def __init__(self, data, key):
+        # Load JSON data from a file
+        # with open('config/config.json') as f:
+        #     self.data = json.load(f)
+        self.data = data
+        self.openai_api_key = key
 
-    # Extract the location of the .env.local file from the JSON data
-    env_location = data['env_location']
+        # # Extract the location of the .env.local file from the JSON data
+        # self.env_location = self.data['env_location']
+        #
+        # # Load the .env.local file from the extracted location
+        # load_dotenv(self.env_location)
+        #
+        # # Now you can access the environment variables using os.environ.get()
+        # self.openai_api_key = os.environ.get('OPENAI_API_KEY')
 
-    # Load the .env.local file from the extracted location
-    load_dotenv(env_location)
+        self.client = OpenAI(api_key=self.openai_api_key)
 
-    # Now you can access the environment variables using os.environ.get()
-    openai_api_key = os.environ.get('OPENAI_API_KEY')
-
-    client = OpenAI(api_key=openai_api_key)
-
-    return client
-
-def get_answer_from_chatgpt(question, context):
-    client = make_connection()
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": context},
-            {"role": "user", "content": question}
-        ]
-    )
-    return response.choices[0].message.content
+    def get_answer_from_chatgpt(self, question, context):
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": context},
+                {"role": "user", "content": question}
+            ]
+        )
+        return response.choices[0].message.content
