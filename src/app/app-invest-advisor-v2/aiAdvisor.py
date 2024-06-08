02d7edc08eba7d92a7dc2ecfff91f6f45f6d2af9
joeylam <joey.cm.lam@gmail.com>
2024-06-08 18:33:55 +0800

    reflector

--- src/app/app-invest-advisor-v2/aiAdvisor.py
+++ src/app/app-invest-advisor-v2/aiAdvisor.py
@@ -1,19 +1,15 @@
 from openai import OpenAI
 
 class AIAdvisor:
-    def __init__(self, data, key):
-        # Load JSON data from a file
-        # with open('config/config.json') as f:
-        #     self.data = json.load(f)
+    def __init__(self, data, key, model):
         self.data = data
         self.openai_api_key = key
-
-
+        self.model = model
         self.client = OpenAI(api_key=self.openai_api_key)
 
     def get_answer_from_chatgpt(self, question, context):
         response = self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model=self.model,
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": context},
