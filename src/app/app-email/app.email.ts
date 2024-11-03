55af43389945e86d7c86eef8ecf2cc21e656bda9
joeylam <joey.cm.lam@gmail.com>
2024-11-03 17:20:51 +0800

    update some info

--- src/app/app-email/app.email.ts
+++ src/app/app-email/app.email.ts
@@ -1,7 +1,7 @@
-import Config from "./Config";
-import {EmailSender} from "./EmailSender";
+import { Config } from './Config';
+import { EmailSender } from './EmailSender';
 
-const config = new Config('config.json');
+const config = new Config('src/app/app-email/config/test.json');
 const emailConfig = config.getEmailConfig();
 
 const emailSender = new EmailSender();
