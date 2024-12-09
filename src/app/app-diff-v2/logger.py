eafd995cc22fb95394a3da0648c8ded2e1a07f80
joeylam <joey.cm.lam@gmail.com>
2024-12-09 10:39:53 +0800

    reflector logger

--- src/app/app-diff-v2/logger.py
+++ src/app/app-diff-v2/logger.py
@@ -1,6 +1,8 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from datetime import datetime
 import os
+
 
 class Logger:
     """
@@ -8,30 +10,32 @@ class Logger:
     """
     def __init__(self, log_config):
         log_path = log_config.get("path", "./log")
-        log_file = log_config.get("file", "diff.log")
-        self.logger = self._initialize_logger(log_path, log_file)
+        log_file = log_config.get("file", "logfile")
+        log_level = log_config.get("level", "INFO").upper()
+        enable_console = log_config.get("enable_console", True)
+        self.logger = self._initialize_logger(log_path, log_file, log_level, enable_console)
 
     @staticmethod
-    def _initialize_logger(log_path, log_file):
+    def _initialize_logger(log_path, log_file, log_level, enable_console):
         os.makedirs(log_path, exist_ok=True)
 
-        # Add timestamp to the log file name
-        base_name, ext = os.path.splitext(log_file)
-        timestamped_log_file = f"{base_name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}{ext}"
+        # Create timestamped log file
+        timestamped_log_file = f"{os.path.splitext(log_file)[0]}_{datetime.now():%Y%m%d_%H%M%S}.log"
         log_file_name = os.path.join(log_path, timestamped_log_file)
 
         logger = logging.getLogger("FileComparison")
-        logger.setLevel(logging.INFO)
+        logger.setLevel(getattr(logging, log_level, logging.INFO))
 
-        # File handler
-        file_handler = logging.FileHandler(log_file_name)
+        # File handler with rotation
+        file_handler = RotatingFileHandler(log_file_name, maxBytes=10 * 1024 * 1024, backupCount=5)
         file_handler.setFormatter(logging.Formatter(Logger._log_format()))
         logger.addHandler(file_handler)
 
-        # Console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(logging.Formatter(Logger._log_format()))
-        logger.addHandler(console_handler)
+        # Console handler (optional)
+        if enable_console:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(logging.Formatter(Logger._log_format()))
+            logger.addHandler(console_handler)
 
         return logger
 
