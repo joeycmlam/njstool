ed5cd3b2c8685b81890e87c0f77351f77d3cad71
joeylam <joey.cm.lam@gmail.com>
2025-05-10 23:06:05 +0800

    unrealised and realised P/L

---
+++ src/app/app-pl/test/features/step_definitions/MySteps.ts
@@ -0,0 +1,32 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { expect } from "chai";
+import { MutualFundService, Transaction, ProfitLoss } from "../../../MutualFundService";
+
+let mutualFundService: MutualFundService;
+let profitLoss: ProfitLoss;
+
+Given("I have the following transactions:", function (dataTable) {
+    mutualFundService = new MutualFundService();
+
+    // Parse the data table into transactions and add them to the service
+    const transactions: Transaction[] = dataTable.hashes().map((row: any) => ({
+        date: new Date(row.date),
+        type: row.type as "BUY" | "SELL",
+        units: parseFloat(row.units),
+        price: parseFloat(row.price),
+    }));
+
+    transactions.forEach((transaction) => mutualFundService.addTransaction(transaction));
+});
+
+When("the current market price is {float}", function (currentMarketPrice: number) {
+    profitLoss = mutualFundService.calculateProfitLoss(currentMarketPrice);
+});
+
+Then("the realized profit loss should be {float}", function (expectedRealizedPL: number) {
+    expect(profitLoss.realizedProfitLoss).to.be.closeTo(expectedRealizedPL, 0.01);
+});
+
+Then("the unrealized profit loss should be {float}", function (expectedUnrealizedPL: number) {
+    expect(profitLoss.unrealizedProfitLoss).to.be.closeTo(expectedUnrealizedPL, 0.01);
+});
---
+++ src/app/app-pl/test/features/test.feature
@@ -0,0 +1,11 @@
+Feature: Mutual Fund Profit and Loss Calculation
+
+  Scenario: Calculate realized and unrealized P/L after multiple transactions
+    Given I have the following transactions:
+      | date       | type | units | price |
+      | 2023-01-01 | BUY  | 100   | 10    |
+      | 2023-02-01 | BUY  | 50    | 12    |
+      | 2023-03-01 | SELL | 80    | 15    |
+    When the current market price is 16
+    Then the realized profit loss should be 400
+    And the unrealized profit loss should be 320
