97466098d1b35cc86ccb33a26f5d04e25b3819df
joeylam <joey.cm.lam@gmail.com>
2025-07-20 01:17:09 +0800

    reflector

--- src/app/app-etl-py/app.etl.py
+++ src/app/app-etl-py/app.etl.py
@@ -1,4 +1,3 @@
-import csv
 import json
 import argparse
 import sys
@@ -6,37 +5,8 @@ from businessrulesengine import BusinessRulesEngine
 from rule_loader import RuleLoader
 from logger import ILogger, LoggerFactory
 from configloader import ConfigLoader
+from csvhelper import CsvReader, CsvWriters
 
-class CsvReader:
-    def __init__(self, input_file):
-        self.input_file = input_file
-
-    def read_records(self):
-        with open(self.input_file, newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
-            return list(reader), reader.fieldnames
-
-
-
-class CsvWriters:
-    def __init__(self, categories, fieldnames):
-        self.files = {}
-        self.writers = {}
-        for category in categories:
-            filename = category["output_file"]
-            key = category["name"]
-            f = open(filename, "w", newline="")
-            self.files[key] = f
-            writer = csv.DictWriter(f, fieldnames=fieldnames or [])
-            writer.writeheader()
-            self.writers[key] = writer
-
-    def write(self, category, record):
-        self.writers[category].writerow(record)
-
-    def close(self):
-        for f in self.files.values():
-            f.close()
 
 class ETLProcessor:
     """Simple ETL Processor with logger dependency."""
---
+++ src/app/app-etl-py/csvhelper.py
@@ -0,0 +1,30 @@
+import csv
+
+class CsvReader:
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    def read_records(self):
+        with open(self.input_file, newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            return list(reader), reader.fieldnames
+
+class CsvWriters:
+    def __init__(self, categories, fieldnames):
+        self.files = {}
+        self.writers = {}
+        for category in categories:
+            filename = category["output_file"]
+            key = category["name"]
+            f = open(filename, "w", newline="")
+            self.files[key] = f
+            writer = csv.DictWriter(f, fieldnames=fieldnames or [])
+            writer.writeheader()
+            self.writers[key] = writer
+
+    def write(self, category, record):
+        self.writers[category].writerow(record)
+
+    def close(self):
+        for f in self.files.values():
+            f.close()
